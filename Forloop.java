50137fd823d3180e3b1d91af91eb94cfc95e84d8
rchuhi <94059813+rchuhi@users.noreply.github.com>
2021-11-29 17:02:00 -0500

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,23 @@
+/*Reginah Chuhi
+CIS219
+Tuesday and Thursday 3:30pm*/
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		
+		/*sets the variable to 0 before the loop begins 
+		  Which is less than or equal to 10
+		  and increases a value by 1 every time the loop runs
+		  
+		 */
+		for(int i=0; i<=10; i++) 
+	       {
+	           System.out.println(i);
+	       }
+		}
+		
+
+	}
+
+
